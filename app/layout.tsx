d2eb928124c4796660a93ad7ec7ae51d8724c78b
Ajay Singh <admin@ajaysingh.com.np>
2024-03-30 18:35:59 +0545

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "@/app/globals.css";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/providers/Theme";
 import { packageJson } from "@/lib/package";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Nunito({
+  subsets: ["latin"],
+  weight: ["600", "900"],
+});
 
 export const metadata: Metadata = {
   title: {
     template: `%s | ${packageJson.productName} v${packageJson.version}`,
-    default: `${packageJson.productName} v${packageJson.version}`
+    default: `${packageJson.productName} v${packageJson.version}`,
   },
   description: `${packageJson.description}`,
 };
@@ -23,19 +26,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={cn(
-        "min-h-screen bg-background antialiased",
-        inter.className
-      )}>
+      <body
+        className={cn(
+          "min-h-screen antialiased",
+          "bg-background",
+          // "bg-gradient-to-r from-violet-200 to-pink-200",
+          // "bg-gradient-to-r from-neutral-300 to-stone-400",
+          // "dark:bg-gradient-to-r from-stone-500 to-stone-700",
+          font.className
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <div className="min-h-screen min-w-screen">
-            {children}
-          </div>
+          <div className="min-h-screen min-w-screen">{children}</div>
         </ThemeProvider>
       </body>
     </html>
